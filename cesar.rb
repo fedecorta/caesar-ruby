684bb407f313c4831382f02618cd6e5ee9a1c3bd
Federico <cortabarriafederico@gmail.com>
2024-03-07 23:23:38 -0300

    arregalr upcase y downcase

--- cesar.rb
+++ cesar.rb
@@ -1,4 +1,4 @@
-letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+letters = ["a","A","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K", "l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V","w","W","x", "X","y","Y","z","Z"]
 
 
 def shift_places(string, number, letters)
@@ -6,7 +6,7 @@ def shift_places(string, number, letters)
   split_string.each_with_index do |character, index|
     if letters.include?(character)
       letter_index = letters.find_index(character)
-      new_letter_index = letter_index - number
+      new_letter_index = letter_index - 2*number
       new_letter = letters[new_letter_index]
       split_string[index] = new_letter
     end
@@ -14,4 +14,4 @@ def shift_places(string, number, letters)
   print split_string.join("")
 end
 
-shift_places("aaaaaa", 4, letters)
+shift_places("pepE", 4, letters)
